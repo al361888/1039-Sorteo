342a870218f5c2ab01e4e73f875d90422173ba36
Mackyplz <miguelligeron@gmail.com>
2019-10-10 19:21:03 +0200

    Generador de jugadores donete.

--- src/Demo.java
+++ src/Demo.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Demo {
     public static void main(String[] args) {
         LoteriaPrimitiva loteria = new LoteriaPrimitiva();
+        List<Jugador> jugadores = new ArrayList<Jugador>();
+        Random r = new Random(32);
 
-        Jugador j1 = new Jugador("Juan", Arrays.asList(0,1,2,3,4,5));
-        Jugador j2 = new Jugador("Pepe", Arrays.asList(1,2,3,4,3,3));
-        Jugador j3 = new Jugador("Jose", Arrays.asList(1,2,3,3,3,3));
-        loteria.registerObserver(j1);
-        loteria.registerObserver(j2);
-        loteria.registerObserver(j3);
+        int n = 5;
+        //Crea n jugadores y los anyade a la lista de jugadores
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            for (int x = 0; x < 6; x++) {
+                numbers.add(x, r.nextInt(48) + 1);
+            }
+            Jugador j = new Jugador("Player " + i, numbers);
+            jugadores.add(j);
+        }
 
-
+        //Anyade la lista de jugadores a la loteria
+        for (Jugador jug : jugadores)
+            loteria.registerObserver(jug);
 
         loteria.nuevoSorteo();
     }
